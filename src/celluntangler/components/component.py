75c6fcaeaea86e9260db37ae3d4cd511e7034b3e
sbc806 <sarahc19233@gmail.com>
2024-12-05 11:13:35 +0000

    Adjusted log_likelihood() in vae.py and made it so that likelihood_n can be used when training.

--- src/celluntangler/components/component.py
+++ src/celluntangler/components/component.py
@@ -223,6 +223,16 @@ class EuclideanComponent(Component):
     def true_dim(self) -> int:
         return self.dim
 
+    def init_layers(self, in_dim: int, scalar_parametrization: bool) -> None:
+        self.manifold = self.create_manifold()
+        self.sampling_procedure = self._sampling_procedure_type(self.manifold, scalar_parametrization, in_dim)
+
+        self.fc_mean = torch.nn.Linear(in_dim, self.mean_dim)
+
+        if scalar_parametrization:
+            self.fc_logvar = torch.nn.Linear(in_dim, 1)
+        else:
+            self.fc_logvar = torch.nn.Linear(in_dim, self.true_dim)
 
 class ConstantComponent(Component):
 

fe24959e3200e6b8416361a6b40a4715301decc4
sbc806 <sarahc19233@gmail.com>
2025-04-12 23:05:40 +0000

    Added parameters to use batch for z1 or z2.

--- src/celluntangler/models/nb_vae.py
+++ src/celluntangler/models/nb_vae.py
@@ -137,7 +137,6 @@ class NBVAE(ModelVAE):
 
         if not use_z_batch:
             batch = torch.zeros(batch.shape)
-        print(batch)
         concat_z = torch.concat((concat_z, batch), dim=1)
         x = self.decoder(concat_z)
 
